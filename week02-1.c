06bbc3b952b235e4bf8bff929e595673cbd12036
LANProgramer <112918619+ProgramerLan@users.noreply.github.com>
2022-09-13 12:04:12 +0800

    Add files via upload

---
+++ week02-1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{printf("Hello World\n");
+ printf("H\tello World\n");
+ printf("\thello World\n");
+ }
---
+++ week02-2.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+int main()
+{printf("1250\n");
+ printf("這裡有字，有插入值\n");
+ printf("這裡有字 %d 有插入值\n",1250);
+ }
